4a6a4ccd19960cd2421f766ea33a96a3f39d1fff
mod-san <mod-san@github.com>
2022-05-29 21:39:53 +0300

    add cflags in makefile

---
+++ event_sample_controller_move/controller_move.EV3.d
@@ -0,0 +1,7 @@
+controller_move.EV3.elf: main.c ../event_lib/include/common.h \
+ ../event_lib/include/mem.h ../event_lib/include/event_tbl.h \
+ ../event_lib/include/types.h
+../event_lib/include/common.h:
+../event_lib/include/mem.h:
+../event_lib/include/event_tbl.h:
+../event_lib/include/types.h:
